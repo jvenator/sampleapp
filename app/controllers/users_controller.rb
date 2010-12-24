a6c8261b100922adeada9c0220dfa9bda0fe82c8
slipshot <j@venator.vc>
2010-12-23 19:19:44 -0500

    User registration complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,15 @@ class UsersController < ApplicationController
     @user = User.new
     @title = "Register"
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to Venator Ventures"
+      redirect_to @user
+    else
+      @title = "Register"
+      render 'new'
+    end
+  end
 end

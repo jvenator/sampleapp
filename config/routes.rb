71643c4dad2e05c0c416212a19e3786a2ee9ebb4
slipshot <j@venator.vc>
2011-02-14 18:10:06 -0500

    Added user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Sampleapp::Application.routes.draw do
 
-  resources :users
-  resources :sessions, :only => [:new, :create, :destroy]
-  resources :microposts, :only => [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+    
+  end
+  resources :sessions,      :only => [:new, :create, :destroy]
+  resources :microposts,    :only => [:create, :destroy]
+  resources :relationships, :only => [:create, :destroy]
   
   match '/register',    :to => 'users#new'
   match '/login',       :to => 'sessions#new'
